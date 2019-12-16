52654fca2fb52362532e2674c5c3991bd390374c
mariana-martins <mariana.menezes@kathmandu.co.nz>
2019-12-16 18:35:33 +1300

    Added some comments on JS file

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,4 @@
+// Activate toggle button as active and display a form based on type
 function activateStatus(type) {
     $('#' + type + '-button').addClass('active');
     $('#' + type + '-button').attr('aria-pressed', 'true');
@@ -7,6 +8,7 @@ function activateStatus(type) {
     $('#' + type + '-form').attr('aria-hidden', 'false');
 }
 
+// Deactivate toggle button as not active and hide the form based on type
 function deactivateStatus(type) {
     $('#' + type + '-button').removeClass('active');
     $('#' + type + '-button').attr('aria-pressed', 'false');
@@ -16,6 +18,7 @@ function deactivateStatus(type) {
     $('#' + type + '-form').attr('aria-hidden', 'true');
 }
 
+// Clean all form fields
 function cleanForm() {
     $('.modal .modal-form form input').each(function() {
         $(this).val('');
@@ -25,8 +28,11 @@ function cleanForm() {
     });
 }
 
+// Activate on type and deactivate the opposite type
 function activateType(type) {
     var ariaPressedValue = $('#' + type + '-button').attr('aria-pressed');
+    // Check if the type is not active, if is not, then activate it
+    // It avoid to apply unnecessary changes on the element
     if (ariaPressedValue === 'false') {
         var hideType = type === 'email' ? 'sms' : 'email';
         activateStatus(type);
@@ -35,10 +41,12 @@ function activateType(type) {
     }
 }
 
+// Checks if the field is empty
 function isEmptyField(field) {
     return $(field).val() === '';
 }
 
+// Validate if all fields from a form are filled
 function isFormValid(formId) {
     var valid = true;
     $('#' + formId + ' input, #' + formId + ' textarea').each(function() {
@@ -49,6 +57,9 @@ function isFormValid(formId) {
     return valid;
 }
 
+// Activate or deactivate the submit button based on the state of the form.
+// If it's unfilled, it's deactivate. If totally filled, the button is
+// activated.
 function updateSubmitButtonState(type) {
     var formId = type + '-form';
     if (isFormValid(formId)) {
@@ -58,8 +69,10 @@ function updateSubmitButtonState(type) {
     }
 }
 
+// Display the data from the form on browser console when the form is submitted
 function displayOnSubmit(type) {
     $('#' + type + '-form').on('submit', function(e) {
+        // it prevents propagation as we only want to print form data on console
         e.preventDefault();
         var data = $(
             '#' + type + '-form input, #' + type + '-form textarea'
@@ -68,21 +81,25 @@ function displayOnSubmit(type) {
     });
 }
 
+// init form
+// - add click event to #email-button
 $('#email-button').on('click', function() {
     activateType('email');
 });
-
+// - add click event to #sms-button
 $('#sms-button').on('click', function() {
     activateType('sms');
 });
-
+// - add keydown event to #email-form fields
+//   enable/disable submit button based on form
 $('#email-form input, #email-form textarea').on('keydown', function() {
     updateSubmitButtonState('email');
 });
-
+// - add keydown event to #sms-form fields
+//   enable/disable submit button based on form
 $('#sms-form input, #sms-form textarea').on('keydown', function() {
     updateSubmitButtonState('sms');
 });
-
+// - ensure it print on console form data on submit event
 displayOnSubmit('email');
 displayOnSubmit('sms');
